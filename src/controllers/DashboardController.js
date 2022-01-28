37e062190cb8568fae2dbaf2a681a4b9bff5dce8
Michael <michael0387nascimento@gmail.com>
2022-01-28 16:30:55 -0300

    Profile controller

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -3,11 +3,9 @@ const JobUtils = require('../utils/JobUtils')
 const Profile = require('../model/Profile')
 
 module.exports = {
-  index(req, res) {
-  
+  async index(req, res) {
     const jobs = Job.get()
-    const profile = Profile.get()
-    
+    const profile = await Profile.get()
 
     let statusCount = {
       progress: 0,
@@ -22,25 +20,30 @@ module.exports = {
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? 'done' : 'progress'
 
-      // somando a quantidade de status 
+      // somando a quantidade de status
       statusCount[status] += 1
 
-      
-      jobTotalHours = (status == 'progress')
-        ? jobTotalHours += Number(job["daily-hours"])
-        : jobTotalHours
+      jobTotalHours =
+        status == 'progress'
+          ? (jobTotalHours += Number(job['daily-hours']))
+          : jobTotalHours
 
       return {
         ...job,
         remaining,
         status,
-        budget: JobUtils.caculateBudget(job, profile["value-hour"])
+        budget: JobUtils.caculateBudget(job, profile['value-hour'])
       }
     })
-    
-    // qtd de horas a trabalhar menos a qtd horas/dias para cada job em proegress
-    const freeHours = profile["hours-per-day"] - jobTotalHours
 
-    return res.render('index', { jobs: updateJobs, profile: profile , statusCount: statusCount, freeHours: freeHours});
-  }  
+    // qtd de horas a trabalhar menos a qtd horas/dias para cada job em proegress
+    const freeHours = profile['hours-per-day'] - jobTotalHours
+
+    return res.render('index', {
+      jobs: updateJobs,
+      profile: profile,
+      statusCount: statusCount,
+      freeHours: freeHours
+    })
+  }
 }
--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,30 +1,30 @@
 const Profile = require('../model/Profile')
 
 module.exports = {
-  index(req, res) {
-    return res.render("profile", { profile: Profile.get() })
+  async index(req, res) {
+    return res.render('profile', { profile: await Profile.get() })
   },
 
-  update(req, res ) {
+  async update(req, res) {
     // req.body para obter os dados
-    const data = req.body      
+    const data = req.body
     // definir quantas semanas tem num ano: 52
     const weeksPerYear = 52
     // remover as semanas de ferias do ano, pegar as semanas de 1 mês
-    const weeksPerMonth = ((weeksPerYear - data["vacation-per-year"]) / 12)
+    const weeksPerMonth = (weeksPerYear - data['vacation-per-year']) / 12
     //  total de horas trabalhadas na semana
-    const weekTotalHours = (data["hours-per-day"] * data["days-per-week"])
+    const weekTotalHours = data['hours-per-day'] * data['days-per-week']
     // total de horas trabalhadas no mês
-    const monthlyTotalHours = (weekTotalHours * weeksPerMonth)
-    // valor da hora 
+    const monthlyTotalHours = weekTotalHours * weeksPerMonth
+    // valor da hora
 
-    const valueHour = (data["monthly-budget"] / monthlyTotalHours)
+    const valueHour = data['monthly-budget'] / monthlyTotalHours
+    const profile = await Profile.get()
 
-
-    Profile.update({
-      ...Profile.get(),
+    await Profile.update({
+      ...profile,
       ...req.body,
-      "value-hour" : valueHour
+      'value-hour': valueHour
     })
 
     return res.redirect('/profile')
