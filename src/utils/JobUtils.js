d7c0f4349d0b184c58b5e2fea06bfcc080e33c74
Michael <michael0387nascimento@gmail.com>
2022-02-01 14:17:48 -0300

    need a fix bugs in diffDays and remainingDay

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -1,20 +1,29 @@
 module.exports = {
-  remainingDays({"total-hours": totalHour, "daily-hours": dailyHours, created_at }) {
+  remainingDays({
+    "total-hours": totalHour,
+    "daily-hours": dailyHours,
+    created_at,
+  }) {
     // cálculo de tempo restante
-    const remainingDays = (totalHour / dailyHours.toFixed())
-    // const remainingDays =  (job["total-hours"] / job["daily-hours"]).toFixed()
-  
-    const createdDate = new Date(created_at)
-    const dueDay = createdDate.getDate() + Number(remainingDays)
-    const dueDateInMs = createdDate.setDate(dueDay)
-  
-    const timeDiffInMs = dueDateInMs - Date.now()
+    const remainingDays = totalHour / dailyHours.toFixed();
+    // const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+
+    console.log(remainingDays);
+
+    const createdDate = new Date(created_at);
+    const dueDay = createdDate.getDate() + Number(remainingDays);
+    const dueDateInMs = createdDate.setDate(dueDay);
+
+    const timeDiffInMs = dueDateInMs - Date.now();
     // transformar milli em dias
-    const dayInMs = 1000 * 60 * 60 * 24
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
 
     //  restam x dias
-    return dayDiff
+    // console.log(dayDiff);
+
+    return dayDiff;
   },
-  caculateBudget: ({"total-hours": totalHour}, valueHour) => valueHour * totalHour
-}
+  caculateBudget: ({ "total-hours": totalHour }, valueHour) =>
+    valueHour * totalHour,
+};
